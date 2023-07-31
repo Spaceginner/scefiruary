ac9c12b3eed5df2af8ab99f2be4a169ccea984aa
Spaceginner <ivan.demian2009@gmail.com>
2023-08-01 00:05:53 +0500

    change structure a bit

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,10 @@
 mod alu;
-mod memory;
+mod instruction;
+
+
+pub struct CPU {
+    registry: [u16; 13],
+    memory: [u8; u16::MAX as usize + 1],
+}
 
 // TODO tests
--- src/memory.rs
+++
@@ -1,5 +0,0 @@
-#[derive(Default, Debug)]
-pub struct Memory {
-    registry: [u16; 13],
-    memory: [u8; u16::MAX + 1],
-}
