192e17d5519d5c9f9e98741111d9070fc232239a
Spaceginner <ivan.demian2009@gmail.com>
2023-08-01 21:50:30 +0500

    add fibonacci example incrementing a counter takes 4 cycles :skull: also, i am sorry for the assembler code

---
+++ examples/fibonacci.rs
@@ -0,0 +1,70 @@
+use std::thread;
+use std::time::Duration;
+use scefiruary::cpu::CPU;
+
+
+/// example program - fibonacci sequence
+const FIBONACCI_PROGRAM: &[u8] = &[
+    // python pseudocode                 // num (byte): asm
+    // a = 0
+    0b00110001, 0b00000000, 0b00000000,  //  1 ( 0): mv 0
+    0b00110000, 0b00001000, 0b00000000,  //  2 ( 3): cp roa rga
+    // b = 1
+    0b00110001, 0b00000000, 0b00000001,  //  3 ( 6): mv 1
+    0b00110000, 0b00001000, 0b00000001,  //  4 ( 9): cp roa rgb
+    // counter = 0
+    0b00110001, 0b00000000, 0b00000000,  //  5 (12): mv 0
+    0b00110000, 0b00001000, 0b00000010,  //  6 (15): cp roa rgc
+    // print(a, counter)
+    // LABEL: fib_loop
+    0b00110000, 0b00000000, 0b00001011,  //  7 (18): cp rga rda
+    0b00110000, 0b00000010, 0b00001100,  //  8 (21): cp rgc rdb
+    // counter += 1
+    0b00110001, 0b00000000, 0b00000001,  //  9 (24): mv 1
+    0b00110000, 0b00000010, 0b00001001,  // 10 (27): cp rgc rob
+    0b00010000,                          // 11 (30): sum
+    0b00110000, 0b00001010, 0b00000010,  // 12 (31): cp roc rgc
+    // check = compare(0x7FFF, a)
+    0b00110000, 0b00000000, 0b00001001,  // 13 (34): cp rga rob
+    0b00110001, 0b01111111, 0b11111111,  // 14 (37): mv 0x7FFF
+    0b00011100,                          // 15 (40): cmpr
+    0b00110000, 0b00001010, 0b00000011,  // 16 (41): cp roc rgd
+    // c = b + a
+    0b00110000, 0b00000001, 0b00001000,  // 17 (44): cp rgb roa
+    0b00010000,                          // 18 (47): sum
+    // a = b
+    0b00110000, 0b00000001, 0b00000000,  // 19 (48): cp rgb rga
+    // b = c
+    0b00110000, 0b00001010, 0b00000001,  // 20 (51): cp roc rgb
+    // if check != 0: continue
+    0b00110001, 0b00000000, 0b00010010,  // 21 (54): mv fib_loop
+    0b00110000, 0b00001000, 0b00001001,  // 22 (57): cp roa rob
+    0b00110000, 0b00000011, 0b00001000,  // 22 (63): cp rgd roa
+    0b00110101, 0b00001001, 0b00000110,  // 23 (66): cpnz rob rip
+
+    0b00000001,                          // 24 (67): hlt
+];
+// ...i should prob write a compiler, even if it is for ASM o_o
+// i just was debugging an emulator, thinking *it* was the issue
+// but apparently not, i just messed up the instruction when was "compiling" the program
+// even emulated CPUs are always correct
+
+
+const FREQUENCY: f64 = 15.0;
+
+
+fn main() {
+    let mut cpu = CPU::default();
+    cpu.load_memory(0, FIBONACCI_PROGRAM);
+
+    let mut cycles = 0_u32;
+    while let Ok(display) = cpu.tick() {
+        cycles += 1;
+
+        println!("RDA: {:>5}  RDB: {:>5}  CYCLES: {}", display.0, display.1, cycles);
+
+        thread::sleep(Duration::from_secs_f64(1.0 / FREQUENCY));
+    };
+
+    println!("\nCPU HALTED");
+}
